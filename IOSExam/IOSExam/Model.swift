b5fda7e5b2e0939ed49ad604598494960bcf0219
Mounitha Vemula <88839594+VEMULA-MOUNITHA@users.noreply.github.com>
2023-11-13 17:18:13 -0600

    Committed files

---
+++ IOSExam/IOSExam/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  IOSExam
+//
+//  Created by Mounitha Vemula on 11/13/23.
+//
+
+import Foundation
+
+struct Student {
+    var fullName : String
+    var transferCredits : Double
+    var earliestGraduation : Int
+    var latestGraduation : Int
+}
+
---
+++ IOSExam/IOSExam/StudentTVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSExam
+//
+//  Created by Mounitha Vemula on 11/13/23.
+//
+
+import UIKit
+
+class StudentTVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var studentsTV: UITableView!
+    
+}
+
